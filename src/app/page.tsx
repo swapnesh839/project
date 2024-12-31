a6ad1b85b622939ca545ce776d810ccaef0fdfd0
swapnesh <swapnesha839@gmail.com>
2025-01-01 00:05:52 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-600 to-purple-600 flex items-center justify-center text-white">
+    <div className="min-h-screen bg-gradient-to-r p-8 from-indigo-600 to-purple-600 flex items-center justify-center text-white">
       <div className="text-center space-y-6">
         <p className="text-4xl font-bold tracking-tight -lg">
           Select your destination below
@@ -21,7 +21,7 @@ export default function Home() {
         <p className="text-[20px] font-medium">
           Other Next js Projects i Want to Share
         </p>
-        <div className="flex  justify-center space-x-3">
+        <div className="flex items-center flex-col md:flex-row justify-center space-y-3 md:space-y-0 md:space-x-3">
           <Button
             className="text-white px-6 py-3 rounded-lg bg-indigo-600 hover:bg-indigo-600/70 hover:scale-[1.05] font-semibold transition-all duration-500 shadow-md border border-indigo-700"
             onClick={() => window.open("https://three.swapnesh.icu/", "_blank")}
@@ -39,7 +39,7 @@ export default function Home() {
           Note : Still Working on My Shopping App
         </p>
         <p className="text-[20px] font-medium">GitHub Repos</p>
-        <div className="flex  justify-center space-x-3">
+        <div className="lex items-center flex-col md:flex-row justify-center space-y-3 md:space-y-0 md:space-x-3">
           <Button
             className="text-white px-6 py-3 rounded-lg bg-indigo-600 hover:bg-indigo-600/70 hover:scale-[1.05] font-semibold transition-all duration-500 shadow-md border border-indigo-700"
             onClick={() =>
